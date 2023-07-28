6aa548bb8bca81b023647bd8fc7181c4d72723ff
Tom Sherman <the.tomsherman@gmail.com>
2023-07-28 09:30:33 +0100

    Add electron import

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,10 +1,15 @@
 import { Counter } from "./Counter.js";
+import electron from "electron";
 
 export default function App({ name = "Anonymous" }) {
   return (
     <div style={{ border: "3px red dashed", margin: "1em", padding: "1em" }}>
       <h1>Hello {name}!!</h1>
-      <h3>This is a server component. {Math.random()}</h3>
+      <h3>
+        This is a server component running on Electron v
+        {electron.app.getVersion()}.
+      </h3>
+      <p>{Math.random()}</p>
       <Counter />
     </div>
   );
